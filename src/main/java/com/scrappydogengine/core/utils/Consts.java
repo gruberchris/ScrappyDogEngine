5e291e487c3dfed1343a4b7fd96a893f7af7d2ef
Chris Gruber <chris@chrisgruber.com>
2021-09-28 19:49:37 -0500

    tutorial 12: add Material class

--- src/main/java/com/scrappydogengine/core/utils/Consts.java
+++ src/main/java/com/scrappydogengine/core/utils/Consts.java
@@ -1,7 +1,9 @@
 package com.scrappydogengine.core.utils;
 
+import org.joml.Vector4f;
+
 public class Consts {
     public static final String TITLE = "Scrappy Dog Engine";
-
     public static final float MOUSE_SENSITIVITY = 0.2f;
+    public static final Vector4f DEFAULT_COLOR = new Vector4f(1.0f, 1.0f, 1.0f, 1.0f);
 }
