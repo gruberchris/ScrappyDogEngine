4ea2da6058835db9eb3a02dbad35573cf8576db2
Chris Gruber <chris@chrisgruber.com>
2021-09-30 19:24:26 -0500

    Tutorial #14: implement point light in fragment shader

--- src/main/resources/shaders/fragment.glsl
+++ src/main/resources/shaders/fragment.glsl
@@ -20,11 +20,21 @@ struct DirectionalLight {
     float intensity;
 };
 
+struct PointLight {
+    vec3 color;
+    vec3 position;
+    float intensity;
+    float constant;
+    float linear;
+    float exponent;
+};
+
 uniform sampler2D textureSampler;
 uniform vec3 ambientLight;
 uniform Material material;
 uniform float specularPower;
 uniform DirectionalLight directionalLight;
+uniform PointLight pointLight;
 
 vec4 ambientC;
 vec4 diffuseC;
@@ -61,6 +71,17 @@ vec4 calcLightColor(vec3 light_color, float light_intensity, vec3 position, vec3
     return diffuseColor + specularColor;
 }
 
+vec4 calcPointLight(PointLight pointLight, vec3 position, vec3 normal) {
+    vec3 light_direction = pointLight.position - position;
+    vec3 to_light_direction = normalize(light_direction);
+    vec4 light_color = calcLightColor(pointLight.color, pointLight.intensity, position, to_light_direction, normal);
+
+    // attenuation
+    float distance = length(light_direction);
+    float attenuationInv = pointLight.constant + pointLight.linear * distance + pointLight.exponent * distance * distance;
+    return light_color / attenuationInv;
+}
+
 vec4 calcDirectionalLight(DirectionalLight directionalLight, vec3 position, vec3 normal) {
     return calcLightColor(directionalLight.color, directionalLight.intensity, position, normalize(directionalLight.direction), normal);
 }
@@ -69,6 +90,7 @@ void main() {
     setupColors(material, textureCoordFragment);
 
     vec4 diffuseSpecularComp = calcDirectionalLight(directionalLight, fragPos, fragNormal);
+    diffuseSpecularComp += calcPointLight(pointLight, fragPos, fragNormal);
 
     fragmentColor = ambientC * vec4(ambientLight, 1) + diffuseSpecularComp;
 }
